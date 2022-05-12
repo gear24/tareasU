6be4a918925080c1cd659f193b44ec0e7a7fefc5
gear24 <70075268+gear24@users.noreply.github.com>
2022-05-12 13:05:19 -0600

    faltan los de keny

---
+++ ActividadProgra/animaciones/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace animaciones;
+
+public class animacion
+{
+	string opc = @"
+      ******************************************************
+      *  1. Lados de un triangulo.                         *
+      *  2. Mayor, medio y menor de 3 numeros.             *
+      *  3. Calcular 3 notas.                              *
+      *  4. Determinar si un numero es negativo o postivo. *
+      *  5. Determinar si un numero es par o impar.        *
+      *  6. Salario mas comision de ventas.                *
+      *  7. Calcular la edad de una persona.               *
+      *  8. Tabla de multiplicar dada por el usuario.      *
+      *  9. Determinar si un numero es primo o no.         *
+      *  10. Notacion Sigma.                               *
+      *  11. Resolver la raiz cuadrada de un numero x.     *
+      *  12. Convertir F° a C°.                            *
+      *  13. Calcular un numero y su potencia.             *
+      *  14. Formula cuadratica.                           *
+      ******************************************************";
+
+ 
+
+}
